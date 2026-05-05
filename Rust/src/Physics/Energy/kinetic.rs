3915e529b6c09e36d019cc8dc3ec15ce8cd37190
chickengod2009 <jason.camden.brown@gmail.com>
2026-05-05 14:18:17 -0500

    Update kinetic.rs

--- Rust/src/Physics/Energy/kinetic.rs
+++ Rust/src/Physics/Energy/kinetic.rs
@@ -6,9 +6,40 @@ pub struct KE{
     mass : unit,
     value : unit,
     velocity : unit,
-    h: i32
+
 
 
 }
 //f
 
+impl KE{
+	fn new(mas: unit) -> Self{
+        Self{
+            mass: mas,
+            value: 0 as unit,
+            velocity: 0 as unit
+            }
+    }
+    fn with_vel(mut self, vel: unit) -> Self{
+    	self.velocity = vel;
+        self.value = vel*vel*self.mass*(.5 as unit);
+        self
+    }
+    fn change_vel(&mut self, vel:unit) -> unit{
+    	self.velocity = vel;
+        self.value = vel*vel*self.mass*(.5 as unit);
+        self.velocity
+    }
+    fn mass(&self) -> unit{
+        self.mass.copy()
+    }
+    fn velocity(&self) -> unit{
+        self.velocity.copy()
+    }
+    fn value(&self)->unit{
+        self.value.copy()
+    }
+    
+        
+
+}
