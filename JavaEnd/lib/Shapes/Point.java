684927667c51dfeb8fbac7037db62ba0c99e34b8
chickengod2009 <jason.camden.brown@gmail.com>
2026-05-05 11:00:59 -0500

    Create Point.java

---
+++ JavaEnd/lib/Shapes/Point.java
@@ -0,0 +1,14 @@
+public class Point{
+
+
+  public Point(double a, double b){
+
+    
+
+  }  
+
+
+  private static native long create(double a, double b);
+  private static native void destroy(long a);
+
+}
